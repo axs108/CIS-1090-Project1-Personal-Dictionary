ab3d4c92841ef6a688dbe5b77325fafe66743932
axs108 <112596296+axs108@users.noreply.github.com>
2022-10-30 18:33:43 -0700

    Audreys hw

--- personal-dictionary.js
+++ personal-dictionary.js
@@ -1,1 +1,12 @@
-//Fill in this empty file
+let words = [];
+
+
+function search(word){
+    return words.includes(word);
+}
+
+function addWord(word){
+    words.push(word);
+}
+
+export {search, addWord};
