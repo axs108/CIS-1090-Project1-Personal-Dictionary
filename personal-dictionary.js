3205f1f1c215074df9dff42bbc476c86fa60b50f
axs108 <112596296+axs108@users.noreply.github.com>
2022-11-01 18:25:29 -0700

    Audreys Personal Dictionary

--- personal-dictionary.js
+++ personal-dictionary.js
@@ -1,12 +1,19 @@
-let words = [];
+let words = ["apple", "banana", "circle", "dolphin", "elf", "frog", "great", "hello", "internet", "joke", "kangaroo", "lump", "monkey", "narwhal" ];
 
 
-function search(word){
-    return words.includes(word);
+function search(word) {
+    for (let i = 0; i < words.length; i++) {
+        if (words[i] === (word) ) {
+            return words.includes(word);
+        } 
+    }
+   return false;
 }
 
 function addWord(word){
     words.push(word);
+    words.splice(words.length, 0, (word));
+
 }
 
 export {search, addWord};
